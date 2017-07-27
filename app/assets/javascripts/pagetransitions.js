aae6444fe81d2e339a7eeaad1c36b5b96d6729a0
Seong Jun <gmkseta@gmail.com>
2017-07-27 18:58:51 +0000

    귀찮다 그냥 쓰자...

---
+++ app/assets/javascripts/pagetransitions.js
@@ -0,0 +1,438 @@
+var PageTransitions = (function() {
+
+	var $main = $( '#pt-main' ), //전체 디비전 대충 여따 넣나본데 제이쿼리 객체로? 
+	
+		$pages = $main.children( 'div.pt-page' ),
+		//여긴 그 아래 애들 나눠서  오 그러네 그래서 div 각 페이지가 
+		//유사 배열로 들어감 
+		
+		$iterate = $( '#iterateEffects' ),//여긴 그 버튼이 j쿼리 객체로
+		
+		animcursor = 1,// 커서 1
+		
+		pagesCount = $pages.length,//유사배열 길이 들어감 총 페이지 갯수 여기선 6
+		
+		current = 0,
+		
+		isAnimating = false,
+		endCurrPage = false,
+		endNextPage = false,
+		
+		animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},//이거 약간 해쉬 넣는거같다.
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		
+		//뭔지 모르겠는데 무튼 animEndEventName 에 animationend가 들어감
+		//Modernizr.prefixed 보면.
+		//ƒ (a,b,c){return b?F(a,b,c):F(a,"pfx")} 이케 되어있는데
+		//무튼 잘모르겠음
+		
+		// support css animations
+		support = Modernizr.cssanimations;
+		//무튼 여기에도 true 값이 들어감
+	
+	function init() {//초기화해주는 함수인가봄
+		
+		
+		
+		//jquery 객체에서 each문?? 객체 하나하나 돌는건가 그럼?
+		$pages.each( function() {
+			var $page = $( this );//일단 그 객체 page 에다가 넣고서 
+			//각각 돌면서 거기에 ...아 왼쪽이 키고 오른쪽이 벨류인데..
+			//저거 약간 해쉬같은데? 일단 클릭했을떄 오리지날 원래의 클래스를 넣어주는데.
+			//page.data가 뭔지 모르겠는데 거따가 오리지날클래스 리스트라는 키에 
+			//현재 돌고있는 태그의 클래스를 넣어줌
+			$page.data( 'originalClassList', $page.attr( 'class' ) );
+		} );
+		//	$page.data( 'originalClassList')이케하니까 원래 태그 튀어나오네
+		//함수 생겨먹은건 좀 복잡해보임 
+	
+
+
+		//eq는 또뭐냐.
+		//ƒ (e){
+		//var t=this.length
+		//n = + e + ( 0 > e ? t : 0 ) ;
+		//return this.pushStack(n>=0&&t>n?[this[n]]:[])
+		//}
+		//인데
+		//
+		$pages.eq( current ).addClass( 'pt-page-current' );
+		//아니 근데 저 eq가 사실 그거같은데 그 자식들 eq(3) 하면 (0부터라서) 4번째꺼 말하는거같은데
+		//아 current 얘가 선언한거구나,...current 0 으로 해놨으니까..
+		//무튼 얘 덕에 첫번쨰 페이지가 current로 바로박힘
+		$( '#dl-menu' ).dlmenu( {
+			animationClasses : { in : 'dl-animate-in-2', out : 'dl-animate-out-2' },
+			onLinkClick : function( el, ev ) {
+				ev.preventDefault();
+				nextPage( el.data( 'animation' ) );
+			}
+		} );
+
+
+		//이게 버튼 클릭되었을때 실행되는 거임
+		$iterate.on( 'click', function() {
+			if( isAnimating ) {//뭔가 이게 애니매이션 중일떄는 그냥 false 리턴해버리는듯
+				return false;
+			}
+			if( animcursor > 67 ) {//애니매이션 67개있는데 넘어가면 1로
+				animcursor = 1;
+			}//nextPage라는 함수로.. 현재 애니매이션 매개변수로... 저함수 바로 아래있네
+			nextPage( animcursor );
+			++animcursor;//실행하고나면 다른 애니매이션 보여주게 증가연산자
+		} );
+
+	}
+
+	function nextPage(options ) {
+		var animation = (options.animation) ? options.animation : options;
+		//왜이짓거리 하는지 모르겠지만 어쩃든 여긴 animcursor 들어가는데 일단 처음이니까 1임
+		
+		if( isAnimating ) {//애니매이션 중이면 반환
+			return false;
+		}
+
+		isAnimating = true;//자 이제 바꾼다! 애니매이션!
+		
+		var $currPage = $pages.eq( current );//최근 페이지 객체를 열로넘김
+
+		if(options.showPage){//왜? 저거 머지 왜 그냥 매개변수같은거 아니였나>?? 
+			if( options.showPage < pagesCount - 1 ) {
+				current = options.showPage;
+			}
+			else {
+				current = 0;
+			}
+		}
+		else{
+			if( current < pagesCount - 1 ) {
+				++current;
+			}
+			else {
+				current = 0;
+			}
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+			outClass = '', inClass = '';
+
+		switch( animation ) {
+
+			case 1:
+				outClass = 'pt-page-moveToLeft';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 2:
+				outClass = 'pt-page-moveToRight';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 3:
+				outClass = 'pt-page-moveToTop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 4:
+				outClass = 'pt-page-moveToBottom';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 5:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromRight pt-page-ontop';
+				break;
+			case 6:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromLeft pt-page-ontop';
+				break;
+			case 7:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromBottom pt-page-ontop';
+				break;
+			case 8:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromTop pt-page-ontop';
+				break;
+			case 9:
+				outClass = 'pt-page-moveToLeftFade';
+				inClass = 'pt-page-moveFromRightFade';
+				break;
+			case 10:
+				outClass = 'pt-page-moveToRightFade';
+				inClass = 'pt-page-moveFromLeftFade';
+				break;
+			case 11:
+				outClass = 'pt-page-moveToTopFade';
+				inClass = 'pt-page-moveFromBottomFade';
+				break;
+			case 12:
+				outClass = 'pt-page-moveToBottomFade';
+				inClass = 'pt-page-moveFromTopFade';
+				break;
+			case 13:
+				outClass = 'pt-page-moveToLeftEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 14:
+				outClass = 'pt-page-moveToRightEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 15:
+				outClass = 'pt-page-moveToTopEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 16:
+				outClass = 'pt-page-moveToBottomEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 17:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromRight pt-page-ontop';
+				break;
+			case 18:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromLeft pt-page-ontop';
+				break;
+			case 19:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromBottom pt-page-ontop';
+				break;
+			case 20:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromTop pt-page-ontop';
+				break;
+			case 21:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-scaleUpDown pt-page-delay300';
+				break;
+			case 22:
+				outClass = 'pt-page-scaleDownUp';
+				inClass = 'pt-page-scaleUp pt-page-delay300';
+				break;
+			case 23:
+				outClass = 'pt-page-moveToLeft pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 24:
+				outClass = 'pt-page-moveToRight pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 25:
+				outClass = 'pt-page-moveToTop pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 26:
+				outClass = 'pt-page-moveToBottom pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 27:
+				outClass = 'pt-page-scaleDownCenter';
+				inClass = 'pt-page-scaleUpCenter pt-page-delay400';
+				break;
+			case 28:
+				outClass = 'pt-page-rotateRightSideFirst';
+				inClass = 'pt-page-moveFromRight pt-page-delay200 pt-page-ontop';
+				break;
+			case 29:
+				outClass = 'pt-page-rotateLeftSideFirst';
+				inClass = 'pt-page-moveFromLeft pt-page-delay200 pt-page-ontop';
+				break;
+			case 30:
+				outClass = 'pt-page-rotateTopSideFirst';
+				inClass = 'pt-page-moveFromTop pt-page-delay200 pt-page-ontop';
+				break;
+			case 31:
+				outClass = 'pt-page-rotateBottomSideFirst';
+				inClass = 'pt-page-moveFromBottom pt-page-delay200 pt-page-ontop';
+				break;
+			case 32:
+				outClass = 'pt-page-flipOutRight';
+				inClass = 'pt-page-flipInLeft pt-page-delay500';
+				break;
+			case 33:
+				outClass = 'pt-page-flipOutLeft';
+				inClass = 'pt-page-flipInRight pt-page-delay500';
+				break;
+			case 34:
+				outClass = 'pt-page-flipOutTop';
+				inClass = 'pt-page-flipInBottom pt-page-delay500';
+				break;
+			case 35:
+				outClass = 'pt-page-flipOutBottom';
+				inClass = 'pt-page-flipInTop pt-page-delay500';
+				break;
+			case 36:
+				outClass = 'pt-page-rotateFall pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 37:
+				outClass = 'pt-page-rotateOutNewspaper';
+				inClass = 'pt-page-rotateInNewspaper pt-page-delay500';
+				break;
+			case 38:
+				outClass = 'pt-page-rotatePushLeft';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 39:
+				outClass = 'pt-page-rotatePushRight';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 40:
+				outClass = 'pt-page-rotatePushTop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 41:
+				outClass = 'pt-page-rotatePushBottom';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 42:
+				outClass = 'pt-page-rotatePushLeft';
+				inClass = 'pt-page-rotatePullRight pt-page-delay180';
+				break;
+			case 43:
+				outClass = 'pt-page-rotatePushRight';
+				inClass = 'pt-page-rotatePullLeft pt-page-delay180';
+				break;
+			case 44:
+				outClass = 'pt-page-rotatePushTop';
+				inClass = 'pt-page-rotatePullBottom pt-page-delay180';
+				break;
+			case 45:
+				outClass = 'pt-page-rotatePushBottom';
+				inClass = 'pt-page-rotatePullTop pt-page-delay180';
+				break;
+			case 46:
+				outClass = 'pt-page-rotateFoldLeft';
+				inClass = 'pt-page-moveFromRightFade';
+				break;
+			case 47:
+				outClass = 'pt-page-rotateFoldRight';
+				inClass = 'pt-page-moveFromLeftFade';
+				break;
+			case 48:
+				outClass = 'pt-page-rotateFoldTop';
+				inClass = 'pt-page-moveFromBottomFade';
+				break;
+			case 49:
+				outClass = 'pt-page-rotateFoldBottom';
+				inClass = 'pt-page-moveFromTopFade';
+				break;
+			case 50:
+				outClass = 'pt-page-moveToRightFade';
+				inClass = 'pt-page-rotateUnfoldLeft';
+				break;
+			case 51:
+				outClass = 'pt-page-moveToLeftFade';
+				inClass = 'pt-page-rotateUnfoldRight';
+				break;
+			case 52:
+				outClass = 'pt-page-moveToBottomFade';
+				inClass = 'pt-page-rotateUnfoldTop';
+				break;
+			case 53:
+				outClass = 'pt-page-moveToTopFade';
+				inClass = 'pt-page-rotateUnfoldBottom';
+				break;
+			case 54:
+				outClass = 'pt-page-rotateRoomLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomLeftIn';
+				break;
+			case 55:
+				outClass = 'pt-page-rotateRoomRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomRightIn';
+				break;
+			case 56:
+				outClass = 'pt-page-rotateRoomTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomTopIn';
+				break;
+			case 57:
+				outClass = 'pt-page-rotateRoomBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomBottomIn';
+				break;
+			case 58:
+				outClass = 'pt-page-rotateCubeLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeLeftIn';
+				break;
+			case 59:
+				outClass = 'pt-page-rotateCubeRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeRightIn';
+				break;
+			case 60:
+				outClass = 'pt-page-rotateCubeTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeTopIn';
+				break;
+			case 61:
+				outClass = 'pt-page-rotateCubeBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeBottomIn';
+				break;
+			case 62:
+				outClass = 'pt-page-rotateCarouselLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselLeftIn';
+				break;
+			case 63:
+				outClass = 'pt-page-rotateCarouselRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselRightIn';
+				break;
+			case 64:
+				outClass = 'pt-page-rotateCarouselTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselTopIn';
+				break;
+			case 65:
+				outClass = 'pt-page-rotateCarouselBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselBottomIn';
+				break;
+			case 66:
+				outClass = 'pt-page-rotateSidesOut';
+				inClass = 'pt-page-rotateSidesIn pt-page-delay200';
+				break;
+			case 67:
+				outClass = 'pt-page-rotateSlideOut';
+				inClass = 'pt-page-rotateSlideIn';
+				break;
+
+		}
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+
+	}
+
+	function onEndAnimation( $outpage, $inpage ) {
+		endCurrPage = false;
+		endNextPage = false;
+		resetPage( $outpage, $inpage );
+		isAnimating = false;
+	}
+
+	function resetPage( $outpage, $inpage ) {
+		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	}
+
+	init();
+
+	return { 
+		init : init,
+		nextPage : nextPage,
+	};
+
+})();
