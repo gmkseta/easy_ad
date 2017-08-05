d63b6c8200d0101f9c0464725b0e05911b8c8aac
Kimm <kmdeen23@gmail.com>
2017-08-05 12:41:06 +0000

    조용히해 이채은

--- db/migrate/20170805085958_update_post.rb
+++
@@ -1,4 +0,0 @@
-class UpdatePost < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805085958) do
+ActiveRecord::Schema.define(version: 20170801104650) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "detail"
     t.text     "address"
     t.string   "picture"
+    t.string   "final"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
