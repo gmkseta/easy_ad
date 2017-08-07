690ef6b1281ec025dc477bfe29811c6a4ce6e269
Kimm <kmdeen23@gmail.com>
2017-08-07 16:01:58 +0000

    d

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170805131122) do
     t.text     "detail"
     t.text     "address"
     t.string   "picture"
+    t.string   "final"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
