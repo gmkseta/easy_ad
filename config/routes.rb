8a9bb693d2f91a1064a595cfbdeb03642243bc93
Kimm <kmdeen23@gmail.com>
2017-08-08 16:54:36 +0000

    ㅏ는 커밋의 노예... 이건 testkk파일을 만든 직후다! 스크립트 자체에는 문제가 없당 / 아마 음 좀 첫번째에 적용 이런게 문제되는듯

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'home/list'
   get 'home/edit_test'
   post '/write2' => 'home#write2'
+  
+  get '/testkk' => 'hposts#testkk'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
